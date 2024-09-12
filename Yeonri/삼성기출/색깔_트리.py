c41fc3388c8d5ceaeb1af2b5726e2cca1814411d
yeonri <yeonri0927@gmail.com>
2024-09-12 16:43:48 +0900

    삼성기출 / 색깔트리
    total_score (모든 노드중 하나의 노드 선택) -> calculate_value (선택된 노드에 연결된 모든 자식노드들 확인)
    위의 코드를 사용하면 최상위 노드에서 최하위 노드까지 탐색을 한 후, 다시 다음 노드로 이동하여 최하위 노드까지 탐색한다.
    이 때문에 같은 노드가 계속 탐색되게 되며, 최악의 경우 100,000 * 100,000 번 탐색을 하게 된다. 모든 노드에 대해서 탐색하여 노드들을 중복으로 탐색하도록 만드는 것이 아닌 한번만 탐색을 하도록 만든다.
    기존처럼 자식들의 색을 반환 받은 후, 그 자리에서 점수를 계산하도록 변경을 하였다.

--- Yeonri/삼성기출/색깔_트리.py
+++ Yeonri/삼성기출/색깔_트리.py
@@ -7,6 +7,7 @@
 
 from collections import defaultdict
 
+
 def create_node(content):
     global graph, re_graph
     # 부모 노드의 깊이보다 현재 노드의 깊이가 크면 안됨 > 같거나 작다.
@@ -14,20 +15,23 @@ def create_node(content):
         graph[content[1]] = content[2:]
         re_graph[content[2]].append(content[1])
 
+        # 현재 노드를 루트 노드들에 저장한다.
+        root_node.append(content[1])
+
     else:
         # 부모들을 찾아 이동해서 값을 탐색한다.
         create_flag = True
         next_id = content[2]
 
         create_flag = count_depth(next_id, 2)
-        
+
         if create_flag:
             graph[content[1]] = content[2:]
             re_graph[content[2]].append(content[1])
 
-def count_depth(current_id, cnt):
 
-    parent_id =  graph[current_id][0]
+def count_depth(current_id, cnt):
+    parent_id = graph[current_id][0]
 
     # 현재 깊이보다 cnt가 작을 때, 실패
     if graph[current_id][2] < cnt:
@@ -36,10 +40,10 @@ def count_depth(current_id, cnt):
     # print(current_id, graph[current_id], parent_id, cnt)
     if parent_id == -1:
         return True
-    
+
     return count_depth(parent_id, cnt + 1)
 
-    
+
 def change_color(current_id, col):
     global graph
 
@@ -49,11 +53,12 @@ def change_color(current_id, col):
 
     if current_id not in re_graph:
         return
-    
+
     next_id = re_graph[current_id]
 
     for id in next_id:
         change_color(id, col)
+
 
 def check_color(content):
     print(graph[content[1]][1])
@@ -72,21 +77,48 @@ def calculate_value(current_id):
     for child_id in re_graph[current_id]:
         child_colors = calculate_value(child_id)
         colors.update(child_colors)
-    
+
     return colors
+
 
 # 각 노드에 대한 고유 색상값을 가져와서 점수 계산
 def total_score():
     total = 0
+
     for node_id in graph:
         value = len(calculate_value(node_id))
         total += value ** 2
     return total
 
+
+# 모든 노드들을 돌면서 선택된 노드의 모든 자식들을 다시 탐색하게 된다.
+# 최악의 경우 O(100000 * 100000)
+
+# 이를 개선하기 위해서 모든 노드들을 선택해서 다시 모든 자식들을 탐색는 것을 막아야 한다.
+# 하나하나의 노드에 대해서 자식들의 colors를 전부 반환 받아 그 자리에서 점수를 조회한다.
+
+# dfs를 이용해서 자식들의 컬러들을 반환 받은 후, 점수를 계산한다.
+
+def dfs_score(node):
+    global total
+
+    # 현재 노드의 색을 추가한다.
+    colors = set()
+    colors.add(graph[node][1])
+
+    # 모든 자식 노드들의 색을 반환 받아서 현재 노드의 색깔 딕셔너리에 추가한 후, 점수를 계산한다.
+    for child_node in re_graph[node]:
+        colors |= (dfs_score(child_node))
+
+    total += len(colors) ** 2
+    return colors
+
+
 Q = int(input())
 
 graph = defaultdict(list)
 re_graph = defaultdict(list)
+root_node = []
 
 lst = []
 total = 0
@@ -107,7 +139,11 @@ for content in lst:
         check_color(content)
 
     if content[0] == 400:
-        print(total_score())
+        total = 0
+        for node in root_node:
+            dfs_score(node)
+        print(total)
+        # print(total_score())
 
 # print(graph)
 # print(re_graph)
